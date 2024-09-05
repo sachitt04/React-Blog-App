7c977b4548e7a1d2f22f21723edb4833ac13e775
sachit <sachitforsocials@gmail.com>
2024-09-05 13:26:21 +0530

    create config.js and auth.js

---
+++ src/appwrite/auth.js
@@ -0,0 +1,69 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    async createAccount ({email,password,name}) {
+        try{
+     const userAccount = await this.account
+     .create(ID.unique(), email,password,name)
+
+     if(userAccount){
+
+        return this.login({email,password})
+
+     } else{
+        userAccount;
+     }
+
+
+        } catch(err){
+            console.log("error ::", err)
+
+        }
+    }
+
+    async login ({email,password}){
+try {
+      return await this.account.createEmailPasswordSession(
+                 email,
+                password
+                )
+} catch (err) {
+
+    console.log("error in login section::",err)
+    
+    }
+  }
+
+  async getCurrentUser() {
+    try{
+        return await this.account.get()
+    } catch(err){
+        console.log("error while getting current user::",err)
+    }
+  }
+
+  async logout (){
+    try {
+        await this.account.deleteSessions();
+    } catch (error) {
+        console.log("ERror while logging out ::",err)       
+    }
+  }
+} 
+
+
+const authService = new AuthService()
+
+
+export default AuthService
