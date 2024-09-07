3534698c3b7317ca1188b32fa0f34d20e6975e37
sachit <sachitforsocials@gmail.com>
2024-09-07 14:14:17 +0530

    createbutton componenet

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Container = ({Children}) => {
+  return 
+    <div className='w-full max-w-7xl mx-auto px-4'>
+        {Children}</div>;
+  
+}
+
+export default Container
